83db540bb0f8a6226407f48a126a91f292ba8e52
GabrielSantosAlmeida15 <gabrielsantosalmeida15@gmail.com>
2025-02-05 14:54:55 -0300

    Criação de variaveis, entrada de dados e saida de dados

--- 05-02/script.js
+++ 05-02/script.js
@@ -1,2 +1,12 @@
-console.log("teste");
-document.write("Bem vindo ao Senai");
+let nome, idade, cidade, time
+
+nome = prompt("Qual o seu nome?");
+idade = prompt("Quantos anos você tem?");
+cidade = prompt("Qual cidade você mora?");
+time = prompt("Para Qual time você torce?");
+
+document.write("Seu nome é:  " + nome + " você tem: " + idade + " Anos ");
+document.write("<br>");
+document.write("Voce mora na cidade de " + cidade + ", e torce para o time " + time); 
+document.write("<br><br>");
+document.write(`Seja bem vindo ${nome}, sua idade é ${idade} e você mora na cidade de ${cidade} torce para o ${time}`);
