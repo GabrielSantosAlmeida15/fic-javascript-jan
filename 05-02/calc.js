e714fae177f2c136c205680d6e38d5c2eeae7485
GabrielSantosAlmeida15 <gabrielsantosalmeida15@gmail.com>
2025-02-05 16:16:19 -0300

    Fazendo Exercicio

--- 05-02/calc.js
+++ 05-02/calc.js
@@ -5,4 +5,14 @@
 let num1 = Number(prompt("Digite um numero"));
 let num2 = Number(prompt("Digite outro numero"));
 
-console.log(`A soma dos numeros é ${num1 + num2}`);
+// Operações
+let soma = num1 + num2;
+let sub = num1 - num2;
+let mult = num1 * num2;
+let div = num1 / num2
+
+// imprimir resposta
+console.log(`A soma dos numeros é ${soma}`);
+console.log(`A subtração dos numeros é ${sub}`);
+console.log(`A multiplicação dos numeros é ${mult}`);
+console.log(`A divisão dos numeros é ${div}`);
---
+++ 05-02/exercicio.js
@@ -0,0 +1,9 @@
+//Exercicio
+
+let tom = "Tom";
+let firstTom = "Hanks";
+let secondTom = "Cruise";
+let myFavoriteTom = `${tom}, ${firstTom}, ${secondTom} `;
+
+console.log(myFavoriteTom);
+
