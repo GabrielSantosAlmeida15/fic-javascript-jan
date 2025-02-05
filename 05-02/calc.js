8986bd893c64414378a2b06a4baef814bd9b0426
GabrielSantosAlmeida15 <gabrielsantosalmeida15@gmail.com>
2025-02-05 15:45:09 -0300

    fazendo calculos

---
+++ 05-02/calc.js
@@ -0,0 +1,8 @@
+// Calculadora
+// Num1 e Num2
+//Pergunta para o usuario
+
+let num1 = Number(prompt("Digite um numero"));
+let num2 = Number(prompt("Digite outro numero"));
+
+console.log(`A soma dos numeros é ${num1 + num2}`);
