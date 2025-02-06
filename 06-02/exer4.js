24fb30127d6f95ceb62c50e58ff93923429d9923
GabrielSantosAlmeida15 <gabrielsantosalmeida15@gmail.com>
2025-02-06 15:49:27 -0300

    Adcionando exer5

---
+++ 06-02/exer4.js
@@ -0,0 +1,7 @@
+const mozilla = "Mozilla Developer Network";
+
+// Imprimir MDN
+
+let palavra = `${mozilla[0]}${mozilla[8]}${mozilla[18]}`;
+
+console.log(palavra);
---
+++ 06-02/exer5.js
@@ -0,0 +1,9 @@
+/*
+- exiba no console do browser a quantidade de caracteres que a contante const possui
+*/
+
+const linguagem = "JavaScript";
+
+palavra = linguagem.length
+
+console.log(palavra);
