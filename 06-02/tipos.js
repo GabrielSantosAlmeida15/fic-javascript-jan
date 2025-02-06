bebcb7569acb288beeec97e1c0e2939d5a5eb1b2
GabrielSantosAlmeida15 <gabrielsantosalmeida15@gmail.com>
2025-02-06 14:41:25 -0300

    Adicionando tipos de dados

---
+++ 06-02/tipos.js
@@ -0,0 +1,24 @@
+// String
+
+let nome = "Gabriel";
+
+
+// Inteiro
+
+let idade = 23;
+
+// Booleano
+
+let casado = false;
+
+// Números flutuantes com casas decimais
+
+let valorProduto = 5.35;
+
+console.log("Meu nome é: ",nome);
+
+console.log("Idade: ", idade);
+
+console.log("Casado? ", casado);
+
+console.log("R$", valorProduto);
---
+++ 06-02/variavel.js
@@ -0,0 +1,6 @@
+let mensagem = "Olá, mundo";
+const nomeCompleto = "Gabriel S Almeida";
+let idade = 23;
+
+console.log(nomeCompleto);
+console.log(`${mensagem} e idade é ${idade}`);
