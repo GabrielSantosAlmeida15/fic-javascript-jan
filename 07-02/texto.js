30ed3afbdfba201ab7c1430920d4eadccbeee71f
GabrielSantosAlmeida15 <gabrielsantosalmeida15@gmail.com>
2025-02-07 14:27:02 -0300

    Adicionando pasta 07-02 e revisando conteudo anterior

---
+++ 07-02/texto.js
@@ -0,0 +1,10 @@
+"use strict";
+
+let text = "Estão chegando as provas";
+
+console.log(text.length);
+
+console.log(text.toLowerCase());
+console.log(text.toUpperCase());
+
+console.log(text.replace("provas", "avaliações").toUpperCase());
