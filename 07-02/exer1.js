628b86c485951f4c285d7481e5d3864131a1f71d
GabrielSantosAlmeida15 <gabrielsantosalmeida15@gmail.com>
2025-02-07 15:23:35 -0300

    Fzd exercicios

---
+++ 07-02/exer1.js
@@ -0,0 +1,17 @@
+/**
+ * use o método adequado para adicionar os seguintes itens
+ * - Coxinha
+ * - Null
+ * - Brigadeiro
+ */
+
+let brazilianFoods = [];
+
+brazilianFoods.push("Coxinha", null, "Brigadeiro");
+
+console.log(brazilianFoods);
+
+brazilianFoods[1] = "Pão de queijo"
+
+
+console.log(brazilianFoods);
