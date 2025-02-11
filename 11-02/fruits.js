d2af41d2446d3da6d00585ed2c2767360b8b318e
GabrielSantosAlmeida15 <gabrielsantosalmeida15@gmail.com>
2025-02-11 14:59:28 -0300

    Adcicionando exercicio fruits

---
+++ 11-02/fruits.js
@@ -0,0 +1,11 @@
+const fruits = ["morango", "banana", "mamão", "pera"];
+
+if(fruits.includes("abacaxi") == true){
+    console.log("A string abacaxi existe no arrays fruits");
+}
+else if(fruits.includes("pera") == true){
+    console.log("A string pera existe no arrays fruits");
+}
+else{
+    console.log("Nem pera nem abacaaxi existe no arrays fruits");
+}
