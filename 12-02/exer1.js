d99c59d5ea31277493bde7d784ba730957a57030
GabrielSantosAlmeida15 <gabrielsantosalmeida15@gmail.com>
2025-02-12 14:00:34 -0300

    Refatorando código

--- 12-02/exer1.js
+++ 12-02/exer1.js
@@ -1,6 +1,8 @@
-let age = 67;
+const age = 67;
+const isAgeFirst = age <=7;
+const isAgeLast = age >=65;
 
-if(age <= 7 || age >= 65){
+if(isAgeFirst || isAgeLast){
     console.log("Para você, a entrada é grátis");
 }else{
     console.log("A entrada é R$ 30,00")
