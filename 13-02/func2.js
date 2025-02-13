a3bd4e62ddb2da398cb4128b63696a285dc23bd8
GabrielSantosAlmeida15 <gabrielsantosalmeida15@gmail.com>
2025-02-13 16:21:02 -0300

    Praticando o uso de funções

---
+++ 13-02/func2.js
@@ -0,0 +1,26 @@
+
+let frase = prompt("qaul frase deseja exibir?");
+
+function mostrarNome(nome){
+    document.write(nome);
+}
+
+function pularLinha(){
+    document.write("<br><br>");
+}
+
+function mostrarFrase(frase){
+    document.write(frase);
+}
+
+
+
+// pularLinha();
+// mostrarNome("Gabriel");
+// pularLinha();
+// mostrarNome("Miguel");
+// pularLinha();
+// mostrarNome("Samuel");
+// pularLinha();
+
+mostrarFrase(frase);
