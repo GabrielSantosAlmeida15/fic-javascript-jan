41d5424f0fcff4d82289cb04eb4372d28af57267
GabrielSantosAlmeida15 <gabrielsantosalmeida15@gmail.com>
2025-02-14 16:15:04 -0300

    Melhorando função resultadoNotaClick

--- 14-02/Js/nota.js
+++ 14-02/Js/nota.js
@@ -3,6 +3,19 @@ const inputNota = document.getElementById("nota");
 const resultado = document.getElementById("resultado");
 
 function resultadoNotaClick(event){
+    // previne um comportamento no navegador
     event.preventDefault();
     console.log(inputNota.value);
+
+    // condição
+    if(inputNota.value >= 6){
+        console.log("Aluno Aprovado");
+        resultado.innerHTML = "Aluno Aprovado"
+    }
+    else{
+        console.log("Aluno Reprovado");
+        resultado.innerHTML = "Aluno Reprovado"
+    }
+
+    inputNota.value = "";
 }
