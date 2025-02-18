68ec9a1267ace27998e1c7571a71eccead26d4e2
GabrielSantosAlmeida15 <gabrielsantosalmeida15@gmail.com>
2025-02-18 14:36:45 -0300

    Adicionando funcionalidade do display

--- 14-02/Js/nota.js
+++ 14-02/Js/nota.js
@@ -1,6 +1,6 @@
 // Váriaveis
-const inputNota = document.getElementById("nota");
-const resultado = document.getElementById("resultado");
+const inputNota = document.getElementById("nota")
+const resultado = document.getElementById("resultado")
 let messageError = document.getElementById("message");
 
 function resultadoNotaClick(event){
@@ -8,16 +8,21 @@ function resultadoNotaClick(event){
     event.preventDefault();
     console.log(inputNota.value);
 
+    resultado.innerHTML = ""
+
     // Validação de formulario
     if(inputNota.value == ""){
         messageError.style.display= "block";
         return false;
     }
 
+    messageError.style.display= "none";
+
     // condição
     if(inputNota.value >= 6){
         console.log("Aluno Aprovado");
         resultado.innerHTML = "Aluno Aprovado"
+        
     }
     else{
         console.log("Aluno Reprovado");
