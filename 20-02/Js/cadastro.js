134d6b4bad31a6fe60af42f5b2de7c6f355c27bc
GabrielSantosAlmeida15 <gabrielsantosalmeida15@gmail.com>
2025-02-20 14:42:28 -0300

    Criando funcionalidade para salvar valor no local storage

---
+++ 20-02/Js/cadastro.js
@@ -0,0 +1,18 @@
+//Variáveis
+const campo = document.querySelector("#campo");
+const saveBtn = document.querySelector("#salvar");
+
+//Adicionar evento de clique ao botão salvar
+saveBtn.addEventListener("click", function(){
+    const value = campo.value;
+
+    //Salvar no local storage
+    const key = `ValorSalvo_${Date.now()}`; // usa timestamp para chave única
+    
+    //Funcionalidade do LocalStorage
+    localStorage.setItem(key, value);
+
+    //Limpar Campo após salvar
+
+    campo.value = "";
+});
