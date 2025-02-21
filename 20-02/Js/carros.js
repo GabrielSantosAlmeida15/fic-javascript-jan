4348bdcaed325c564c6a66f85873c2794868aa27
GabrielSantosAlmeida15 <gabrielsantosalmeida15@gmail.com>
2025-02-21 16:13:44 -0300

    Finalizando card dos cars

--- 20-02/Js/carros.js
+++ 20-02/Js/carros.js
@@ -8,9 +8,65 @@ const cars = [
         brand: "Honda",
         model: "Civic",
         year: 2008
+    },
+    {
+        brand: "BMW",
+        model: "X6",
+        year: 2021
+    },
+    {
+        brand: "Ford",
+        model: "Ranger",
+        year: 2022
+    },
+    {
+        brand: "Fiat",
+        model: "FastBack",
+        year: 2022
+    },
+    {
+        brand: "Chevrolet",
+        model: "S10",
+        year: 2015
+    },
+    {
+        brand: "Volkswagen",
+        model: "Jeta",
+        year: 2022
     }
 ];
 
-console.log(cars[0].model);
-console.log(cars[1].year);
-console.log(cars[0].model)
+//Cria uma função e chama essa função displayCards, aqui mesmo
+
+function displayCards(){
+    // Variável
+    const cardList = document.querySelector("#card-list");
+
+    cars.forEach((car) => {
+        //Criamos um elemento div pelo javascript
+        const cardDiv = document.createElement("div")
+        // Criamos uma class css cars
+        cardDiv.classList.add('car');
+
+        //Criando o modelo 
+        const carModel = document.createElement("h2");
+        carModel.textContent = `${car.brand} ${car.model}`
+
+        //Criando o ano
+        const carYear = document.createElement("p");
+        carYear.textContent = `Ano: ${car.year}`;
+
+        // Imprimir na tela as informações
+        // elementoPai.appendChild(elementoFilho)
+        cardDiv.appendChild(carModel);
+
+        cardList.appendChild(cardDiv);
+
+        cardDiv.appendChild(carYear);
+
+        
+    })
+
+}
+
+displayCards();     
