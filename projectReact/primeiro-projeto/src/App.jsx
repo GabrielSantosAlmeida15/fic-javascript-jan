0da9d84588946aa21c1eefef8984e7248ace8e4b
GabrielSantosAlmeida15 <gabrielsantosalmeida15@gmail.com>
2025-02-25 16:07:19 -0300

    Criando Components footer e header

--- projectReact/primeiro-projeto/src/App.jsx
+++ projectReact/primeiro-projeto/src/App.jsx
@@ -1,10 +1,19 @@
+import Header from "./Components/Header"
+
 function App() {
 
   return (
     <>
-      <p>
-        Gabriel
-      </p>
+      <div className="container">
+        
+        <Header />
+
+        <h1>Seja bem Vindo</h1>
+
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consectetur eius amet obcaecati minus repudiandae quasi repellat facere maxime,
+           dolorem id sed fuga nulla necessitatibus error, nihil sit illo deserunt iure!
+        </p>
+      </div>
     </>
   )
 }
